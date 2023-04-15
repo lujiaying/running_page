7c30971897506821ac52d8c56ffb6759f75b92fd
jiaying.lu <lujiaying93@foxmail.com>
2023-04-15 09:50:53 -0400

    add running data so far

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,14 @@
 module.exports = {
-  pathPrefix: process.env.PATH_PREFIX || '/',
+  pathPrefix: `/running`,
   siteMetadata: {
     siteTitle: 'Running Page',
-    siteUrl: 'https://yihong.run',
+    siteUrl: 'https://lujiaying.github.io',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-    description: 'Personal site and blog',
+    description: 'Personal site for running records',
     navLinks: [
       {
-        name: 'Blog',
-        url: 'https://yihong.run/running',
-      },
-      {
-        name: 'About',
-        url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+        name: "Jiaying's Personal Webpage",
+        url: 'https://lujiaying.github.io/',
       },
     ],
   },
